5106d1ea97da9f999f028572adeb0f4b92b5f0ba
Olexij <hereisalexius@gmail.com>
2018-11-02 20:48:57 +0200

    after initializr

---
+++ src/main/java/com/hereisalexius/userscrud/UsersCrudApplication.java
@@ -0,0 +1,12 @@
+package com.hereisalexius.userscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersCrudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hereisalexius/userscrud/UsersCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hereisalexius.userscrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
